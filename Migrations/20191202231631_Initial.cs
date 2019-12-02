3e9c6e98e497557ccf7847ed4fdc31af5f599a19
JoaoMoledoDias <40269391+JoaoMoledoDias@users.noreply.github.com>
2019-12-02 23:21:03 +0000

    Migrações e Update da Database

---
+++ Migrations/20191202231631_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestaoTarefasIPG.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionario",
+                columns: table => new
+                {
+                    FuncionarioId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 50, nullable: true),
+                    Servico = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Telemovel = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionario", x => x.FuncionarioId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Funcionario");
+        }
+    }
+}
