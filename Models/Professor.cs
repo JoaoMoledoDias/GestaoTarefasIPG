b4c095d007d404db2042467703a932ff83456a21
Francisco-Cordeiro <39482628+Francisco-Cordeiro@users.noreply.github.com>
2019-12-03 00:32:28 +0000

    Criação do modelo Professor e Scafollding do controlador Professor

---
+++ Models/Professor.cs
@@ -0,0 +1,19 @@
+﻿namespace GestaoTarefasIPG.Models
+{
+    public class Professor
+    {
+        public int ProfessorID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telemovel { get; set; }
+
+        public string Numero_De_Professor { get; set; }
+
+        public string Numero_De_Gabinete { get; set; }
+
+        public string  Unidade_Curricular { get; set; }
+    }
+}
