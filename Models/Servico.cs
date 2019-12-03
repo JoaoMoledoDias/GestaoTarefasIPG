19542c733625742059368b0f980e56c62a0086be
vitalpgoncalves <vital.pedro.edu@gmail.com>
2019-12-03 14:46:08 +0000

    Criação e Edição do Model (Servico); ID, Nome, Descrição, Localização;

---
+++ Models/Servico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class Servico
+    {
+        public int ServicoId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Localizacao { get; set; }
+    }
+}
