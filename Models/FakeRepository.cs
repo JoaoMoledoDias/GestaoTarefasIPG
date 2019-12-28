fe29ec7359ce96777cf12a55a29a3a8d4f02ca26
Francisco-Cordeiro <39482628+Francisco-Cordeiro@users.noreply.github.com>
2019-12-28 14:31:11 +0000

    criação mudificação dos modelos/classes/interface:  -ProfessorsController  -FakeRepository  -ITarefasIPGRepository  -Startup

---
+++ Models/FakeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class FakeRepository : ITarefasIPGRepository
+    {
+        public IEnumerable<Professor> Professor => new List<Professor> { 
+
+            new Professor {Nome="", Email="", Telemovel="", Numero_De_Professor="", Numero_De_Gabinete=""}
+
+        };
+
+        public IEnumerable<Funcionario> Funcionario => new List<Funcionario> { 
+        
+            new Funcionario {Nome="", Email="", Telemovel="", Servico=""}
+
+        };
+
+        public IEnumerable<Servico> Servico => new List<Servico> {
+        
+            new Servico{Nome="", Descricao="", Localizacao=""}
+
+        };
+
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,11 @@ namespace GestaoTarefasIPG
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            /*------------------------------------------------------------------------*/
+            services.AddMvc();
+            services.AddTransient<ITarefasIPGRepository, FakeRepository>();
+            /*------------------------------------------------------------------------*/
+           
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
