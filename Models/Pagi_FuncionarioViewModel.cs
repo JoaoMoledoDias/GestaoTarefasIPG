f945074f66f80ef17b17c0941af07526d3df9783
JoaoMoledoDias <40269391+JoaoMoledoDias@users.noreply.github.com>
2020-01-07 14:40:52 +0000

    Paginação no Modelo Funcionarios

---
+++ Models/Pagi_FuncionarioViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class Pagi_FuncionarioViewModel
+    {
+        public IEnumerable<Funcionario> Funcionario { get; set; }
+        public int PaginaAtual { get; set; }
+
+        public int TotalPaginas { get; set; }
+
+        public int PrimeiraPagina { get; set; }
+
+        public int UltimaPagina { get; set; }
+
+        public string StringProcura { get; set; }
+    }
+}
